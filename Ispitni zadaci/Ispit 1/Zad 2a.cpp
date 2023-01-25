e3b4805e7441b1c9bbca757cac037b11746fc2a4
Emin-Brankovic <115090589+Emin-Brankovic@users.noreply.github.com>
2023-01-25 23:41:53 +0100

    Create Zad 2a.cpp

---
+++ Ispitni zadaci/Ispit 1/Zad 2a.cpp
@@ -0,0 +1,36 @@
+/*Napisati program koji će omogućiti korisniku unos broja n (uz uslov 10 <= n <= 1000). Zatim simulirati unos n slučajnih vrijednosti.
+Simuliranje unosa ostvariti funkcijom rand() % 1000 + 1. 
+Izračunati statističke podatke u kojem procentu se od n generisanih slučajnih vrijednosti pojavljuje prost broj.
+Npr. Ako je generisano 10 brojeva (n = 10): 5,7,4,13,17,20,25,23,30,45, u ovom uzorku od 10 brojeva postoji 5 prostih brojeva pa je procenat prostih brojeva u ovom slučaju 50%.*/
+
+#include <iostream>
+#include <time.h>
+using namespace std;
+bool isProst(int broj);
+
+void main()
+{ 
+	srand(time(0));
+	int n,broj;
+	float brojac = 0;
+	do
+	{
+		cout << "Unijeti n broj ponavljanja: ", cin >> n;
+	} while (n<10 || n>1000);
+	for (int i = 0; i < n; i++)
+	{
+		broj = rand() % 1000 + 1;
+		if (isProst(broj))
+			brojac++;
+	}
+
+	cout << "Procenat prosti brojeva u " << n << " broju ponavljanja je: " << round(((brojac / n * 100)))<<"%" << endl;
+}
+
+bool isProst(int broj)
+{
+	if (broj < 2) return false;
+	for (int i = 2; i < broj / 2; i++)
+		if (broj % i == 0) return false;
+	return true;
+}
