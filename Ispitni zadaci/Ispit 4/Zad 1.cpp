5f0067a498e38a6e88b021ea36aabcd5ecc21db2
Emin-Brankovic <115090589+Emin-Brankovic@users.noreply.github.com>
2023-01-26 17:39:22 +0100

    Create Zad 1.cpp

---
+++ Ispitni zadaci/Ispit 4/Zad 1.cpp
@@ -0,0 +1,20 @@
+/*Napisati program u kojem je potrebno unijeti odabrani broj cifara počevši od cifre najveće težinske vrijednosti, pa od njih sastaviti i ispisati prirodni broj. 
+Unos cifara se prekida kada se unese broj manji od 0 ili veći od 9. */
+#include <iostream>
+
+using namespace std;
+
+
+void main()
+{
+	int cifra,broj=0;
+	do
+	{
+		cout << "Cifra: ", cin >> cifra;
+		if (cifra >= 0 && cifra <= 9)
+		{
+			broj = broj * 10 + cifra;
+		}
+	} while (cifra>=0 && cifra<=9);
+	cout << broj;
+}
